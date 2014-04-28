8b26139da30909ce085305b8cee4eca9e67b92cc
Robin Böhm <robinboehm@googlemail.com>
2014-04-28 13:43:02 +0200

    refactor(hoodieArray): move helper function into the serviceScope
    no need for a global definition

--- src/hoodieArray.js
+++ src/hoodieArray.js
@@ -1,4 +1,3 @@
-// TODO: factory would be also okay
 angular.module('hoodie')
   .factory('hoodieArray',
 function(hoodieStore, hoodie) {
@@ -67,57 +66,60 @@ function(hoodieStore, hoodie) {
     });
   };
 
+
+  /**
+   * Creates a map out of an array be choosing what property to key by
+   * @param {object[]} array Array that will be converted into a map
+   * @param {string} prop Name of property to key by
+   * @return {object} The mapped array. Example:
+   *     mapFromArray([{a:1,b:2}, {a:3,b:4}], 'a')
+   *     returns {1: {a:1,b:2}, 3: {a:3,b:4}}
+   */
+  function mapFromArray(array, prop) {
+    var map = {};
+    for (var i = 0; i < array.length; i++) {
+      map[ array[i][prop] ] = array[i];
+    }
+    return map;
+  }
+
+  /**
+   * @param {object[]} o old array of objects
+   * @param {object[]} n new array of objects
+   * @param {object} An object with changes
+   */
+  function getDelta(o, n, comparator) {
+    var delta = {
+      added: [],
+      deleted: [],
+      changed: []
+    };
+    var mapO = mapFromArray(o, 'id');
+    var mapN = mapFromArray(n, 'id');
+    var id;
+    for (id in mapO) {
+      if (!mapN.hasOwnProperty(id)) {
+        delta.deleted.push(mapO[id]);
+      } else if (!comparator(mapN[id], mapO[id])) {
+        delta.changed.push(mapN[id]);
+      }
+    }
+
+    // New attributes cannot be mapped by id as the id is undefined and
+    // there may be more than 1 new item
+    for (k in n) {
+      if (!n[k]['id']) { // new?
+        delta.added.push(n[k]);
+      }
+    }
+
+    return delta;
+  }
+
+
   // Public API
   return {
     bind: service.bind
   };
 });
 
-/**
- * Creates a map out of an array be choosing what property to key by
- * @param {object[]} array Array that will be converted into a map
- * @param {string} prop Name of property to key by
- * @return {object} The mapped array. Example:
- *     mapFromArray([{a:1,b:2}, {a:3,b:4}], 'a')
- *     returns {1: {a:1,b:2}, 3: {a:3,b:4}}
- */
-function mapFromArray(array, prop) {
-  var map = {};
-  for (var i = 0; i < array.length; i++) {
-    map[ array[i][prop] ] = array[i];
-  }
-  return map;
-}
-
-/**
- * @param {object[]} o old array of objects
- * @param {object[]} n new array of objects
- * @param {object} An object with changes
- */
-function getDelta(o, n, comparator) {
-  var delta = {
-    added: [],
-    deleted: [],
-    changed: []
-  };
-  var mapO = mapFromArray(o, 'id');
-  var mapN = mapFromArray(n, 'id');
-  var id;
-  for (id in mapO) {
-    if (!mapN.hasOwnProperty(id)) {
-      delta.deleted.push(mapO[id]);
-    } else if (!comparator(mapN[id], mapO[id])) {
-      delta.changed.push(mapN[id]);
-    }
-  }
-
-  // New attributes cannot be mapped by id as the id is undefined and
-  // there may be more than 1 new item
-  for (k in n) {
-    if (!n[k]['id']) { // new?
-      delta.added.push(n[k]);
-    }
-  }
-
-  return delta;
-}
