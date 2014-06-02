2ec383828697300b7527419762be922c7ae45877
Robin Böhm <robinboehm@googlemail.com>
2014-06-02 09:00:50 +0200

    chore(reformat) use autoformat

--- src/hoodieStore.js
+++ src/hoodieStore.js
@@ -1,53 +1,50 @@
-// TODO: factory would be also okay
 angular.module('hoodie')
   .factory('hoodieStore',
-function($rootScope, $q, hoodie) {
-  var service = {};
+  function ($rootScope, $q, hoodie) {
+    var service = {};
 
-  angular.forEach(hoodie.store, function(propertyValue, propertyName) {
-    if(angular.isFunction(propertyValue)){
-      service[propertyName] = function () {
-        return $q.when(hoodie.store[propertyName].apply(hoodie.account, arguments));
-      };
-    }
-    else{
-      // TODO: Problem by value (copied, ref gets lost)
-      service[propertyName] = hoodie.store[propertyName];
-    }
-  });
-
-  // TODO: Add Comment -> What happens here??
-  // If I'll create an store named findAll this will crash, ya??
-  service.findAll()
-  .then(function (data) {
-    data.forEach(function (item) {
-      var type = item.type;
-      service[type] = service[type] || [];
-      service[type].push(item);
+    angular.forEach(hoodie.store, function (propertyValue, propertyName) {
+      if (angular.isFunction(propertyValue)) {
+        service[propertyName] = function () {
+          return $q.when(hoodie.store[propertyName].apply(hoodie.account, arguments));
+        };
+      }
+      else {
+        // TODO: Problem by value (copied, ref gets lost)
+        service[propertyName] = hoodie.store[propertyName];
+      }
     });
-  });
 
-  // write custom wrapper here, it's more complicated
-  // Events
-  hoodie.store.on('change', function (event, changedObject) {
-    $rootScope.$evalAsync(function() {
-      var type = changedObject.type;
-      var id = changedObject.id;
-
-      $rootScope.$emit(event, changedObject);
-      $rootScope.$emit('change', event, changedObject);
-      $rootScope.$emit(event + ':' + type, changedObject);
-      $rootScope.$emit('change' + ':' + type, event, changedObject);
-      $rootScope.$emit(event + ':' + type + ':' + id, changedObject);
-      $rootScope.$emit('change' + ':' + type + ':' + id, event, changedObject);
-
-      // TODO: Add Comment -> What happens here??
-      service.findAll(type)
+    // If I'll create an store named findAll this will crash, ya??
+    service.findAll()
       .then(function (data) {
-        service[type] = data;
+        data.forEach(function (item) {
+          var type = item.type;
+          service[type] = service[type] || [];
+          service[type].push(item);
+        });
+      });
+
+    // write custom wrapper here, it's more complicated
+    // Events
+    hoodie.store.on('change', function (event, changedObject) {
+      $rootScope.$evalAsync(function () {
+        var type = changedObject.type;
+        var id = changedObject.id;
+
+        $rootScope.$emit(event, changedObject);
+        $rootScope.$emit('change', event, changedObject);
+        $rootScope.$emit(event + ':' + type, changedObject);
+        $rootScope.$emit('change' + ':' + type, event, changedObject);
+        $rootScope.$emit(event + ':' + type + ':' + id, changedObject);
+        $rootScope.$emit('change' + ':' + type + ':' + id, event, changedObject);
+
+        service.findAll(type)
+          .then(function (data) {
+            service[type] = data;
+          });
       });
     });
-  });
 
-  return service;
-});
+    return service;
+  });
